c73cc4fa588b1041c151077785809d746a8875d2
Liubomur Puliak <liubomurpuliak@gmail.com>
2025-03-23 11:43:51 +0200

    fmt2

--- task05/modules/app_service/main.tf
+++ task05/modules/app_service/main.tf
@@ -4,35 +4,35 @@ resource "azurerm_windows_web_app" "app" {
   resource_group_name = var.resource_group_name
   location            = var.location
   service_plan_id     = var.app_service_plan_id
-  
+
   site_config {
     application_stack {
       current_stack  = "dotnet"
       dotnet_version = "v6.0"
     }
-    
+
     dynamic "ip_restriction" {
       for_each = [var.allow_ip_address]
       content {
-        name        = var.allow_ip_rule_name
-        ip_address  = "${ip_restriction.value}/32"
-        action      = "Allow"
-        priority    = 100
+        name       = var.allow_ip_rule_name
+        ip_address = "${ip_restriction.value}/32"
+        action     = "Allow"
+        priority   = 100
       }
     }
-    
+
     dynamic "ip_restriction" {
       for_each = ["AzureTrafficManager"]
       content {
-        name             = var.allow_tm_rule_name
-        service_tag      = ip_restriction.value
-        action           = "Allow"
-        priority         = 200
+        name        = var.allow_tm_rule_name
+        service_tag = ip_restriction.value
+        action      = "Allow"
+        priority    = 200
       }
     }
-    
+
     ip_restriction_default_action = "Deny"
   }
-  
+
   tags = var.tags
 }
--- task05/modules/traffic_manager/main.tf
+++ task05/modules/traffic_manager/main.tf
@@ -3,12 +3,12 @@ resource "azurerm_traffic_manager_profile" "tm" {
   name                   = var.name
   resource_group_name    = var.resource_group_name
   traffic_routing_method = var.routing_method
-  
+
   dns_config {
     relative_name = var.name
     ttl           = 60
   }
-  
+
   monitor_config {
     protocol                     = "HTTPS"
     port                         = 443
@@ -17,15 +17,15 @@ resource "azurerm_traffic_manager_profile" "tm" {
     timeout_in_seconds           = 10
     tolerated_number_of_failures = 3
   }
-  
+
   tags = var.tags
 }
 
 resource "azurerm_traffic_manager_azure_endpoint" "endpoint" {
-  for_each            = var.endpoints
-  name                = each.value.name
-  profile_id          = azurerm_traffic_manager_profile.tm.id
-  target_resource_id  = each.value.target_resource_id
-  weight              = each.value.weight
-  priority            = each.value.priority
+  for_each           = var.endpoints
+  name               = each.value.name
+  profile_id         = azurerm_traffic_manager_profile.tm.id
+  target_resource_id = each.value.target_resource_id
+  weight             = each.value.weight
+  priority           = each.value.priority
 }
